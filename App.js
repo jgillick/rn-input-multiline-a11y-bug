efe57baa9d30a8361992ed50fe902f56488da03c
Jeremy <j_gillick@yahoo.com>
2023-03-13 11:43:17 -0700

    Cleanup and document

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import { StyleSheet, Text, View, TextInput, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
 
-      <Text style={styles.text}>Single-line text field works</Text>
-      <TextInput style={styles.input} />
-
+      <Text style={styles.title}>Multi-line TextInput Accessibility Bug</Text>
       <Text style={styles.text}>
-        Multi-line text field never announces "is editing" or "insertion point"
+        To test this, enable iOS VoiceOver and then activate the next two
+        fields:
       </Text>
-      <TextInput style={styles.input} multiline={true} />
-    </View>
+
+      <View style={styles.fieldContainer}>
+        <Text style={styles.text}>Single-line text field works</Text>
+        <TextInput style={styles.input} placeholder="Single-line text field" />
+      </View>
+      <View style={styles.fieldContainer}>
+        <Text style={styles.text}>
+          Multi-line text field never announces "is editing" or "insertion
+          point"
+        </Text>
+        <TextInput
+          style={[styles.input, styles.inputMultiLine]}
+          multiline={true}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -21,18 +34,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
     marginHorizontal: 20,
+    marginTop: 100,
+    gap: 20,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
   },
   text: {
     marginBottom: 5,
   },
+  fieldContainer: {
+    width: "100%",
+  },
   input: {
     width: "100%",
-    marginBottom: 12,
     padding: 10,
     borderWidth: 1,
     borderRadius: 10,
+  },
+  inputMultiLine: {
+    minHeight: 40,
   },
 });
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# rn-input-multiline-a11y-bug
-Demonstrates the TextInput accessibility bug with React Native 0.71
+# React Native 0.71 multi-line TextInput Accessibility Bug
+
+With React Native 0.71, multi-line TextInput fields are no longer accessible and do not announce "is editing" to assistive devices. This results in assistive device keyboards, like braille screen input, from working and renders the text input entirely unusable.
+
+## Setup
+
+```shell
+yarn install
+yarn expo start
+```
+
+## To Test
+
+This is best tested on an actual iOS device with VoiceOver enabled.
+
+1. Using Expo Go, open the app on your physical iOS device.
+2. Then, go into iOS Settings > Accessibility > VoiceOver and enable VoiceOver
+3. Swipe from the bottom of the screen to the top to enable the app switcher.
+4. Long-tap on the ExpoGo screen to highlight, then double-tap to switch to it.
+5. Long-tap on (or right-swipe to navigate) the first text field, then double-tap.
+6. Verify it says "is editing" and "insertion point..." in the description after double-tapping.
+7. Navigate to the second text field (long-tap or right-swipe), then double tap.
+8. Verify it does not say "is editing" or "insertion point" after double-tapping.
